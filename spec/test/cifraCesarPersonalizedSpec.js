17ddd36bc64de1b6699e9a7a47fddb59a08fcd20
Jean Gonçalves <jeanfpgoncalves@live.com>
2016-06-01 23:29:46 -0300

    Spec: quando enviar a frase "CASA azul" no primeiro parâmentro, retornar "FDVD dcxo"

--- spec/test/cifraCesarPersonalizedSpec.js
+++ spec/test/cifraCesarPersonalizedSpec.js
@@ -1,17 +1,32 @@
 describe("cifraCesar", function() {
 
-	var CrpCsar = require('../../lib/src/cifraCesarPersonalized');
+	var CrpCsar = require('../../lib/dist/cifraCesarPersonalized.min');
 	var crpCsar;
 
 	beforeEach(function() {
 		crpCsar = new CrpCsar();
 	});
 
-	it("retornar NULL quando não enviar parâmetro", function() {
-		expect(crpCsar.cript()).toEqual(null);
+	describe('Criptografia com um parâmetro', function() {
+		it("retornar NULL quando não enviar parâmetro", function() {
+			expect(crpCsar.cript()).toEqual(null);
+		});
+
+		it('quando enviar letra "A" no primeiro parâmentro, retornar "D"', function() {
+			expect(crpCsar.cript('A')).toEqual('D');
+		});
+
+		it('quando enviar letra "B" no primeiro parâmentro, retornar "E"', function() {
+			expect(crpCsar.cript('B')).toEqual('E');
+		});
+
+		it('quando enviar a palavra "CASA" no primeiro parâmentro, retornar "FDVD"', function() {
+			expect(crpCsar.cript('CASA')).toEqual('FDVD');
+		});
+
+		it('quando enviar a frase "CASA azul" no primeiro parâmentro, retornar "FDVD dcxo"', function() {
+			expect(crpCsar.cript('CASA')).toEqual('FDVD');
+		});
 	});
 
-	it('quando enviar letra A no primeiro parâmentro, retornar D', function() {
-		expect(crpCsar.cript('A')).toEqual('D');
-	});
 });
